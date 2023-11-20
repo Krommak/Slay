83ecb5288179f7cb2e57d9ab4d5463d32bd48bc7
Krommak <60076342+Krommak@users.noreply.github.com>
2023-11-21 01:09:36 +0300

    Добавьте файлы проекта.

---
+++ Assets/Source/Scripts/Inject/Installers/LocationInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace Game.Inject.Installers
+{
+    public class LocationInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+
+        }
+    }
+}
